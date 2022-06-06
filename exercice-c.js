1648107ed4486671fffe9c548f1390b97acd64a7
Felipe Barbosa <felipefb2@hotmail.com>
2022-06-05 22:35:56 -0300

    JS_Part2

--- exercice-c.js
+++ exercice-c.js
@@ -1,24 +1,25 @@
-const datecurrent = new Date (current);
-const day = String(datecurrent.getDate()).padStart(2,'0');
-const month = String(datecurrent.getMonth() + 1).padStart(2,'0');
-const year = datecurrent.getFullYear();
-const currentdate= `${day}/${month}/${year}`;
-const click = document.querySelector ("#btn");
-const result = document.querySelector ("h2");
+const CurrentDate = new Date ()
+const day = String(CurrentDate.getDate()).padStart(2,'0')
+const month = String(CurrentDate.getMonth() + 1).padStart(2,'0')
+const year = CurrentDate.getFullYear()
+const currentdate= `${day}/${month}/${year}`
+const click = document.querySelector ("#btn")
+const span = document.querySelector ('h2 > span')
+
 
 click.addEventListener ("click", function(e) {
-    e.preventDefault();
-    const birth = document.getElementById ("box-date").value;
-    let diffInTime = Math.abs(currentdate - birth)
-    console.log(diffInTime);
+    e.preventDefault()
+    const birth = document.getElementById ("box-date").value
+    const birthday = new Date (birth)
+    let diffInTime = Math.abs(CurrentDate.getTime() - birthday.getTime())
+    DaysLife = diffInTime / (1000*60*60*24)
+    var genre = document.querySelector (".genre:checked")
+    if (genre.value == "M") {
+        DaysRemaining = Math.ceil((73.1*365) - DaysLife)  
+    }
+    else {
+        DaysRemaining = Math.ceil((80.1*365) - DaysLife) 
+    }
+    span.innerHTML = DaysRemaining
 });
-
-
-
-
-// const day = String(data.getDate()).padStart(2,'0');
-// const month = String(data.getMonth() + 1).padStart(2,'0');
-// const year = data.getFullYear();
-// const currentdate= `${day}/${month}/${year}`;
-
 
